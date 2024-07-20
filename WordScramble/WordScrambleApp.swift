a19dc236b3542051fb3c8176241d956b6d95df0e
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-20 11:16:27 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Carlos Eduardo Witte on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
